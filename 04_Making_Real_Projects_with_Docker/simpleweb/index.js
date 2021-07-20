2d2785b24eb32ec00cafb823e7d5aef6dc8df830
KOPFYF <fei8@purdue.edu>
2021-07-19 21:35:20 -0400

    Making Real Projects with Docker

--- 04_Making_Real_Projects_with_Docker/simpleweb/index.js
+++ 04_Making_Real_Projects_with_Docker/simpleweb/index.js
@@ -2,12 +2,9 @@ const express = require("express");
 
 const app = express();
 
-
 app.get("/", (req, res) => {
-  res.send("Hi there");
+  res.send("Hello World");
 });
-
-
 
 app.listen(8080, () => {
   console.log("Listening on port 8080");
